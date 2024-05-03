dba0dbc0e769619f5926d09b480a503637908263
anderson Velasquez <andersonvelezca@gmail.com>
2024-05-02 23:24:13 -0500

    add test delete and getAll of client

--- src/test/java/com/fastporte/fastportewebservice/service/ClientServiceImplTest.java
+++ src/test/java/com/fastporte/fastportewebservice/service/ClientServiceImplTest.java
@@ -44,36 +44,36 @@ public class ClientServiceImplTest {
        assertThat(savedClient).isNotNull();
         assertEquals(client, savedClient);
     }
-//
-//    @Test
-//    public void deleteTest() throws Exception {
-//        Long id = 1L;
-//        clientService.delete(id);
-//        verify(clientRepository, times(1)).deleteById(id);
-//    }
-//
-//    @Test
-//    public void getAllTest() throws Exception {
-//        List<Client> list = new ArrayList<>();
-//        list.add(new Client(1L, "Antonio", "Martinez",
-//                "Antonio Martinez", "photo", "am@gmail.com",
-//                "983654313", "Amazonas",
-//                date, "pass321",
-//                "I work with a lot of merchandise"));
-//        list.add(new Client(1L, "Juan", "Perez",
-//                "Juan Perez", "photo", "jp@gmail.com",
-//                "987654312", "Lima",
-//                date, "pass123",
-//                "My business is the most important thing"));
-//        list.add(new Client(1L, "Joselyn Sofia", "Maldonado",
-//                "Joselyn Maldonado", "photo", "jm@gmail.com",
-//                "937028312", "Ica",
-//                date, "pass456",
-//                "I like to travel around my country"));
-//        given(clientRepository.findAll()).willReturn(list);
-//        List<Client> listExpected = clientService.getAll();
-//        assertEquals(listExpected, list);
-//    }
+
+    @Test
+   public void deleteTest() throws Exception {
+        Long id = 1L;
+       clientService.delete(id);
+        verify(clientRepository, times(1)).deleteById(id);
+    }
+
+    @Test
+    public void getAllTest() throws Exception {
+        List<Client> list = new ArrayList<>();
+       list.add(new Client(1L, "Antonio", "Martinez",
+                "Antonio Martinez", "photo", "am@gmail.com",
+                "983654313", "Amazonas",
+               date, "pass321",
+               "I work with a lot of merchandise"));
+        list.add(new Client(1L, "Juan", "Perez",
+               "Juan Perez", "photo", "jp@gmail.com",
+                "987654312", "Lima",
+                date, "pass123",
+                "My business is the most important thing"));
+        list.add(new Client(1L, "Joselyn Sofia", "Maldonado",
+                "Joselyn Maldonado", "photo", "jm@gmail.com",
+                "937028312", "Ica",
+                date, "pass456",
+                "I like to travel around my country"));
+        given(clientRepository.findAll()).willReturn(list);
+       List<Client> listExpected = clientService.getAll();
+        assertEquals(listExpected, list);
+    }
 //
 //    @Test
 //    public void getByIdTest() throws Exception {
